df8890530adf238b544c63f8a4cf484408b05f3c
Dimitrios Mavridis <dmavridis@outlook.com>
2019-07-11 00:18:48 +0100

    Add files via upload

--- network.py
+++ network.py
@@ -1,133 +1,118 @@
-#!/usr/bin/env python
-#title           :Network.py
-#description     :Architecture file(Generator and Discriminator)
-#author          :Deepak Birla
-#date            :2018/10/30
-#usage           :from Network import Generator, Discriminator
-#python_version  :3.5.4 
-
 # Modules
-from keras.layers import Dense
-from keras.layers.core import Activation
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import UpSampling2D
-from keras.layers.core import Flatten
-from keras.layers import Input
-from keras.layers.convolutional import Conv2D, Conv2DTranspose
-from keras.models import Model
-from keras.layers.advanced_activations import LeakyReLU, PReLU
-from keras.layers import add
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Conv2D, Dense, LeakyReLU, BatchNormalization, Flatten, PReLU, Activation, UpSampling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import Input, Model
+
+def wasserstein_loss(y_true, y_pred):
+    return K.mean(y_true*y_pred)
+
+def perceptual_distance(y_true, y_pred):
+    """Calculate perceptual distance, DO NOT ALTER"""
+    y_true *= 255
+    y_pred *= 255
+    rmean = (y_true[:, :, :, 0] + y_pred[:, :, :, 0]) / 2
+    r = y_true[:, :, :, 0] - y_pred[:, :, :, 0]
+    g = y_true[:, :, :, 1] - y_pred[:, :, :, 1]
+    b = y_true[:, :, :, 2] - y_pred[:, :, :, 2]
+    return K.mean(K.sqrt((((512+rmean)*r*r)/256) + 4*g*g + (((767-rmean)*b*b)/256)))
 
 
 # Residual block
 def res_block_gen(model, kernal_size, filters, strides):
-    
-    gen = model
-    
-    model = Conv2D(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same")(model)
-    model = BatchNormalization(momentum = 0.5)(model)
+    model.add(Conv2D(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same"))
+    model.add(BatchNormalization(momentum = 0.5))
     # Using Parametric ReLU
-    model = PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2])(model)
-    model = Conv2D(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same")(model)
-    model = BatchNormalization(momentum = 0.5)(model)
-        
-    model = add([gen, model])
-    
+    model.add(PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2]))
+    model.add(Conv2D(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same"))
+    model.add(BatchNormalization(momentum = 0.5))
     return model
-    
+  
     
 def up_sampling_block(model, kernal_size, filters, strides):
     
     # In place of Conv2D and UpSampling2D we can also use Conv2DTranspose (Both are used for Deconvolution)
     # Even we can have our own function for deconvolution (i.e one made in Utils.py)
     #model = Conv2DTranspose(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same")(model)
-    model = Conv2D(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same")(model)
-    model = UpSampling2D(size = 2)(model)
-    model = LeakyReLU(alpha = 0.2)(model)
-    
+    model.add(Conv2D(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same"))
+    model.add(UpSampling2D(size = 2))
+    model.add(LeakyReLU(alpha = 0.2))
     return model
-
 
 def discriminator_block(model, filters, kernel_size, strides):
-    
-    model = Conv2D(filters = filters, kernel_size = kernel_size, strides = strides, padding = "same")(model)
-    model = BatchNormalization(momentum = 0.5)(model)
-    model = LeakyReLU(alpha = 0.2)(model)
-    
+    model.add(Conv2D(filters = filters, kernel_size = kernel_size, strides = strides, padding = "same"))
+    model.add(BatchNormalization(momentum = 0.5))
+    model.add(LeakyReLU(alpha = 0.2))
     return model
+
 
 # Network Architecture is same as given in Paper https://arxiv.org/pdf/1609.04802.pdf
 class Generator(object):
 
-    def __init__(self, noise_shape):
-        self.noise_shape = noise_shape
+    def __init__(self, shape):
+        self.image_shape =shape
 
     def generator(self):
-        gen_input = Input(shape = self.noise_shape)
-        model = Conv2D(filters = 64, kernel_size = 9, strides = 1, padding = "same")(gen_input)
-        model = PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2])(model)
-        gen_model = model
+        generator = Sequential()
+        generator.add(Conv2D(filters = 64, kernel_size = 9, strides = 1, padding = "same", input_shape=self.image_shape))
+        generator.add(PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2]))
         # Using 16 Residual Blocks
-        for index in range(16):
-            model = res_block_gen(model, 3, 64, 1)
-        model = Conv2D(filters = 64, kernel_size = 3, strides = 1, padding = "same")(model)
-        model = BatchNormalization(momentum = 0.5)(model)
-        model = add([gen_model, model])
-        
+        for _ in range(16):
+            generator = res_block_gen(generator, 3, 64, 1)
+        generator.add(Conv2D(filters = 64, kernel_size = 3, strides = 1, padding = "same"))
+        generator.add(BatchNormalization(momentum = 0.5))
         # Using 2 UpSampling Blocks
-        for index in range(3):
-            model = up_sampling_block(model, 3, 256, 1)
+        for _ in range(3):
+            generator = up_sampling_block(generator, 3, 256, 1)
+        generator.add(Conv2D(filters = 3, kernel_size = 9, strides = 1, padding = "same"))
+        generator.add(Activation('relu'))
+        generator.summary()
         
-        model = Conv2D(filters = 3, kernel_size = 9, strides = 1, padding = "same")(model)
-        model = Activation('tanh')(model)
-        
-        generator_model = Model(inputs = gen_input, outputs = model, name='Generator')
-        generator_model.summary()
-        return generator_model
+        opt = Adam(lr=1E-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+        generator.compile(optimizer=opt, loss=wasserstein_loss, metrics=[perceptual_distance])
+        return generator
 
 # Network Architecture is same as given in Paper https://arxiv.org/pdf/1609.04802.pdf
 class Discriminator(object):
 
-    def __init__(self, image_shape):
-        self.image_shape = image_shape
+    def __init__(self, shape):
+        self.image_shape = shape
     
     def discriminator(self):
+        discriminator = Sequential()
+        discriminator.add(Conv2D(filters = 64, kernel_size = 3, strides = 1, padding = "same", input_shape=self.image_shape))
+        discriminator.add(LeakyReLU(alpha = 0.2))
+        discriminator = discriminator_block(discriminator, 32, 3, 2)
+        discriminator = discriminator_block(discriminator, 64, 3, 1)
+        discriminator = discriminator_block(discriminator, 64, 3, 2)
+        discriminator = discriminator_block(discriminator, 128, 3, 1)
+        discriminator = discriminator_block(discriminator, 128, 3, 2)
+        discriminator = discriminator_block(discriminator, 256, 3, 1)
+        discriminator = discriminator_block(discriminator, 256, 3, 2)
+        discriminator.add(Flatten())
+        discriminator.add(Dense(64))
+        discriminator.add(LeakyReLU(alpha = 0.2))
+        discriminator.add(Dense(1))
+        discriminator.add(Activation('sigmoid'))
+        discriminator.summary()
         
-        dis_input = Input(shape = self.image_shape)
-        
-        model = Conv2D(filters = 64, kernel_size = 3, strides = 1, padding = "same")(dis_input)
-        model = LeakyReLU(alpha = 0.2)(model)
-        
-        model = discriminator_block(model, 32, 3, 2)
-        model = discriminator_block(model, 64, 3, 1)
-        model = discriminator_block(model, 64, 3, 2)
-        model = discriminator_block(model, 128, 3, 1)
-        model = discriminator_block(model, 128, 3, 2)
-        model = discriminator_block(model, 256, 3, 1)
-        model = discriminator_block(model, 256, 3, 2)
-        
-        model = Flatten()(model)
-        model = Dense(64)(model)
-        model = LeakyReLU(alpha = 0.2)(model)
-       
-        model = Dense(1)(model)
-        model = Activation('sigmoid')(model) 
-        
-        discriminator_model = Model(inputs = dis_input, outputs = model, name='Discriminator')
-        discriminator_model.summary()
-        return discriminator_model
-    
-
-def get_gan_network(discriminator, shape, generator):
+        opt = Adam(lr=1E-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+        discriminator.compile(optimizer=opt, loss=wasserstein_loss, metrics=['acc'])
+        return discriminator
+            
+def gan(generator, discriminator, shape):
     discriminator.trainable = False
     gan_input = Input(shape=shape)
     generated_images = generator(gan_input)
     outputs = discriminator(generated_images)
     gan = Model(inputs=gan_input, outputs=[generated_images, outputs])
-#    gan.compile(
-#            loss=['mse', 'binary_crossentropy'],
-#            loss_weights=[1., 1e-3],
-#            optimizer=optimizer,
-#            metrics = {'gan_output':gan_metric}
-#            )
+    g = gan.get_layer(index=1).name
+    d = gan.get_layer(index=2).name
+    
+    gan.compile(loss={g:'mse', d: 'binary_crossentropy'},
+            loss_weights=[1., 1e-3],
+            optimizer='adam',
+            metrics = {g:perceptual_distance, d: 'binary_crossentropy'}
+            )
     return gan
--- train.py
+++ train.py
@@ -4,18 +4,18 @@ import subprocess
 import os
 from PIL import Image
 import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras import layers
 from tensorflow.keras import backend as K
 from tensorflow.keras.callbacks import Callback
 import wandb
 from wandb.keras import WandbCallback
+from tqdm import tqdm
+from network import Generator, Discriminator, gan
+
 
 run = wandb.init(project='superres')
 config = run.config
-
-config.num_epochs = 2#50
-config.batch_size = 32
+config.num_epochs = 1#50
+config.batch_size = 8
 config.input_height = 32
 config.input_width = 32
 config.output_height = 256
@@ -30,13 +30,15 @@ if not os.path.exists("data"):
     subprocess.check_output(
         "mkdir data && curl https://storage.googleapis.com/wandb/flower-enhance.tar.gz | tar xz -C data", shell=True)
 
-config.steps_per_epoch = len(
-    glob.glob(train_dir + "/*-in.jpg")) // config.batch_size
-config.val_steps_per_epoch = len(
-    glob.glob(val_dir + "/*-in.jpg")) // config.batch_size
+#config.steps_per_epoch = len(
+#    glob.glob(train_dir + "/*-in.jpg")) // config.batch_size
+#config.val_steps_per_epoch = len(
+#    glob.glob(val_dir + "/*-in.jpg")) // config.batch_size
+    
+config.steps_per_epoch = 2
+config.val_steps_per_epoch = 20
 
-
-def image_generator(batch_size, img_dir):
+def image_generator_d(batch_size, img_dir):
     """A generator that returns small images and large images.  DO NOT ALTER the validation set"""
     input_filenames = glob.glob(img_dir + "/*-in.jpg")
     counter = 0
@@ -53,7 +55,27 @@ def image_generator(batch_size, img_dir):
             small_images[i] = np.array(Image.open(img)) / 255.0
             large_images[i] = np.array(
                 Image.open(img.replace("-in.jpg", "-out.jpg"))) / 255.0
-        yield (small_images, large_images)
+        yield (small_images.astype('float32'), large_images.astype('float32'))
+        counter += batch_size
+
+def image_generator_g(batch_size, img_dir):
+    """A generator that returns small images and large images.  DO NOT ALTER the validation set"""
+    input_filenames = glob.glob(img_dir + "/*-in.jpg")
+    counter = 0
+    while True:
+        small_images = np.zeros(
+            (batch_size, config.input_width, config.input_height, 3))
+        large_images = np.zeros(
+            (batch_size, config.output_width, config.output_height, 3))
+        random.shuffle(input_filenames)
+        if counter+batch_size >= len(input_filenames):
+            counter = 0
+        for i in range(batch_size):
+            img = input_filenames[counter + i]
+            small_images[i] = np.array(Image.open(img)) / 255.0
+            large_images[i] = np.array(
+                Image.open(img.replace("-in.jpg", "-out.jpg"))) / 255.0
+        yield (small_images.astype('float32'), large_images.astype('float32'))
         counter += batch_size
 
 
@@ -68,10 +90,8 @@ def perceptual_distance(y_true, y_pred):
 
     return K.mean(K.sqrt((((512+rmean)*r*r)/256) + 4*g*g + (((767-rmean)*b*b)/256)))
 
-
-val_generator = image_generator(config.batch_size, val_dir)
+val_generator = image_generator_g(config.batch_size, val_dir)
 in_sample_images, out_sample_images = next(val_generator)
-
 
 class ImageLogger(Callback):
     def on_epoch_end(self, epoch, logs):
@@ -84,30 +104,71 @@ class ImageLogger(Callback):
             "examples": [wandb.Image(np.concatenate([in_resized[i] * 255, o * 255, out_sample_images[i] * 255], axis=1)) for i, o in enumerate(preds)]
         }, commit=False)
 
+
+input_shape = (config.input_width, config.input_height, 3)
+output_shape = (config.output_width, config.output_height, 3)
+
+g = Generator(input_shape).generator()
+d = Discriminator(output_shape).discriminator()
+srgan = gan(g, d, input_shape)
+
+
+for e in range(1, config.num_epochs+1):
+    for step in range(config.steps_per_epoch):
+
+        image_batch_lr, image_batch_hr = next(image_generator_g(config.batch_size, train_dir))
+
+        generated_images_sr = g.predict(image_batch_lr)
+        
+        real_data_Y = np.ones(config.batch_size) - np.random.random_sample(config.batch_size)*0.2
+        fake_data_Y = np.random.random_sample(config.batch_size)*0.2
+        
+        d.trainable = True
+        d_loss_real = d.train_on_batch(image_batch_hr, real_data_Y)
+        d_loss_fake = d.train_on_batch(generated_images_sr, fake_data_Y)
+        d_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
+        
+        image_batch_lr, image_batch_hr = next(image_generator_d(config.batch_size, train_dir))
+        
+        gan_Y = np.ones(config.batch_size) - np.random.random_sample(config.batch_size)*0.2
+        d.trainable = False
+        loss_gan = srgan.train_on_batch(image_batch_lr, [image_batch_hr,gan_Y])
+        
+    gan_loss, _, _, perc_dist, _ = loss_gan
+    wandb.log({
+        'epoch': e,
+        'loss': gan_loss,
+        'perceptual_distance': perc_dist})
+
+#    g.evaluate_generator(generator=image_generator_d, steps=10, callbacks=[perceptual_distance])
+#g.evaluate_generator(image_generator_d(config.batch_size, train_dir), 
+#                     steps=20,
+#                     callbacks=[ImageLogger(),WandbCallback()])
+
+    # Test
     
-
-def superres_model():
-    model = Sequential()
-    model.add(layers.Conv2D(3, (3, 3), activation='relu', padding='same',
-                            input_shape=(config.input_width, config.input_height, 3)))
-    model.add(layers.UpSampling2D())
-    model.add(layers.Conv2D(3, (3, 3), activation='relu', padding='same'))
-    model.add(layers.UpSampling2D())
-    model.add(layers.Conv2D(3, (3, 3), activation='relu', padding='same'))
-    model.add(layers.UpSampling2D())
-    model.add(layers.Conv2D(3, (3, 3), activation='relu', padding='same'))
+#    for _ in range(config.val_steps_per_epoch):
+#        image_batch_lr, image_batch_hr = next(image_generator_d(config.batch_size, val_dir))
+#        val_loss_gan = gan.test_on_batch(image_batch_lr, [image_batch_hr,gan_Y])
+                
     
-    print(model.summary())
-    return model
+    
+#        if e == 1 or e % 5 == 0:
+#            plot_generated_images(e, generator)
+#        if e % 300 == 0:
+#            generator.save('./output/gen_model%d.h5' % e)
+#            discriminator.save('./output/dis_model%d.h5' % e)
+#            gan.save('./output/gan_model%d.h5' % e)
 
-model = superres_model()
-# DONT ALTER metrics=[perceptual_distance]
-model.compile(optimizer='adam', loss='mse',
-              metrics=[perceptual_distance])
-
-model.fit_generator(image_generator(config.batch_size, train_dir),
-                    steps_per_epoch=config.steps_per_epoch,
-                    epochs=config.num_epochs, callbacks=[
-                        ImageLogger(), WandbCallback()],
-                    validation_steps=config.val_steps_per_epoch,
-                    validation_data=val_generator)
+#    
+#    
+## DONT ALTER metrics=[perceptual_distance]
+#model.compile(optimizer='adam', loss='mse',
+#              metrics=[perceptual_distance])
+#
+#model.fit_generator(image_generator(config.batch_size, train_dir),
+#                    steps_per_epoch=config.steps_per_epoch,
+#                    epochs=config.num_epochs, callbacks=[
+#                        ImageLogger(), WandbCallback()],
+#                    validation_steps=config.val_steps_per_epoch,
+#                    validation_data=val_generator)
